b8b022eeea5c25958b6d5e8a3ee4fe5410506e80
condaminetd <david.condaminet@unicaen.fr>
2020-06-21 09:57:10 +0200

    README

--- README.md
+++ README.md
@@ -2,4 +2,23 @@
 
 ~~ *This project was implemented in May 2020* ~~
 
-During the lockdown period, in the Australian countryside, cut off from the Internet, I took 2 hours of my time to code this game in order to entertain my friends and I for several evenings. 
+*During the lockdown period, in the Australian countryside, cut off from the Internet, I took 2 hours of my time to code this game in order to entertain my friends and I for several evenings.*
+
+Execute in a terminal:
+```
+$ python3 pictionary.py
+```
+
+**Rules:**
+* 581 words to guess
+* 2 teams
+* minimum 4 players
+* the players are randomly and evenly assigned to a team
+* first to 10 points wins (2 pts gap)
+* 1 minute to make your teammates guess the word
+
+If the word is found before the time has elapsed, it is possible to stop the countdown and move on to the next guess, simply by using the keyboard shortcut *Ctrl+C*.
+
+In order to play several games with only new words, the words already played are saved in a file *backup/picmemo.txt*. You can clean this file whenever you want. 
+
+
--- pictionary.py
+++ pictionary.py
@@ -3,6 +3,9 @@ import time
 import sys
 import signal
 
+words = list()
+teamNames = list()
+
 abort = False
 def stop(sig, stack):
     global abort
@@ -10,9 +13,6 @@ def stop(sig, stack):
     abort = True
 
 signal.signal(signal.SIGINT, stop)
-
-words = list()
-nomsEquipes = list()
 
 def timer(secs=60):
     global abort
@@ -31,45 +31,64 @@ def timer(secs=60):
         sys.stdout.write('\r{:15}'.format("Time's up!",))
     sys.stdout.flush()
 
-def constitution_equipes(joueurs, nbEquipes):
-    global nomsEquipes
+
+def create_players():
+    time.sleep(1)
+    print("\n..... WHO PLAYS? .....")
+    players = list()
+    ok = False
+    while not ok:
+        try:
+            nbPlayers = int(input("Number of players: "))
+            if nbPlayers >= 4:
+                ok = True
+            else:
+                print("Minimum 4 players")
+        except:
+            print("Please enter a number")
+    for i in range(nbPlayers):
+        players.append(input("Name of Player " + str(i + 1) + ": ")) 
+    return players
+
+def create_teams(players, numberOfTeams=2):
+    global teamNames
     time.sleep(1)
     print("\n..... TEAM CREATION .....")
-    equipes = [[]] * nbEquipes
+    teams = [[]] * numberOfTeams
     i = 0
-    while len(joueurs) > 0:
-        j = random.choice(joueurs)
-        equipes[i % nbEquipes] = equipes[i % nbEquipes] + [j]
+    while len(players) > 0:
+        j = random.choice(players)
+        teams[i % numberOfTeams] = teams[i % numberOfTeams] + [j]
         i += 1
-        joueurs.remove(j)
-    for i, e in enumerate(equipes):
+        players.remove(j)
+    for i, e in enumerate(teams):
         print("Team", i + 1, ':')
         for j in e:
             time.sleep(.5)
             print("\t", j)
-    for eq in range(nbEquipes):
-        nomsEquipes.append(input("Name of the team " + str(eq + 1) + " : ")) 
-    return equipes
+    for eq in range(numberOfTeams):
+        teamNames.append(input("Name of the team " + str(eq + 1) + ": ")) 
+    return teams
 
-def chargementMots():
+def load_words():
     global words
     print("..... LOADING THE WORDS TO GUESS .....")
-    motsDejaJoues = list()
+    wordsAlreadyPlayed = list()
     for w in open("backup/picmemo.txt", 'r').readlines():
-        motsDejaJoues.append(w.strip().lower())
+        wordsAlreadyPlayed.append(w.strip().lower())
         
     basic_words = list()
     for lvl in range(1, 6):
         for w in open("words/picwords" + str(lvl) + ".txt", 'r').readlines():
             m = w.strip().lower()
-            if m not in motsDejaJoues:
+            if m not in wordsAlreadyPlayed:
                 basic_words.append(m)
     words = list(set(basic_words))
     print(len(words), "new words to guess!")
 
 def picking():
     global words
-    words_loc = list()
+    localWords = list()
     skip = 0
     ok = False
     input("PICK A WORD (press ENTER)")
@@ -77,7 +96,7 @@ def picking():
         if len(words) > 0:
             word = random.choice(words)
             words.remove(word)
-            words_loc.append(word)
+            localWords.append(word)
             print(word)
             decision = input("OK (press ENTER) or enter 'skip' (/!\\ " + str(2 - skip) + " skipping left)\n")
             if 'skip' in decision:
@@ -87,73 +106,71 @@ def picking():
         else:
             return None
     with open("backup/picmemo.txt", 'a') as f:
-        for w in words_loc:
+        for w in localWords:
             f.write(w+'\n')
     if not ok:
         return False
     return word
     
-def jeu(equipes, scoreLimite=10):
-    global nomsEquipes
+def play(teams, scoreLimit=10):
+    global teamNames
     print("\n..... LAUNCHING THE GAME .....")
     time.sleep(1)
-    chargementMots()
-    fin = False
-    scores = [0] * len(equipes)
+    load_words()
+    end = False
+    scores = [0] * len(teams)
     i = 0
-    while not fin:
+    while not end:
         print("//////  SCORES  \\\\\\\\\\\\")
         for s, t in enumerate(scores):
-              print("TEAM", nomsEquipes[s], ":", t, "points")
-        j = i % len(equipes)
-        print("\n", nomsEquipes[j], " => YOUR TURN!")
-        team = equipes[j]
+              print("TEAM", teamNames[s], ":", t, "points")
+        j = i % len(teams)
+        print("\n", teamNames[j], " => YOUR TURN!")
+        team = teams[j]
         print(team[0], "is the drawer!")
-        equipes[j] = team[1:] + [team[0]]
+        teams[j] = team[1:] + [team[0]]
         word = picking()
         if word == False:
             i += 1
-            j = i % len(equipes)
+            j = i % len(teams)
             scores[j] += 1
-            if scores[j] >= scoreLimite:
-                verif_ecart = True
-                for u in range(1, len(equipes)):
-                    if not (scores[j] - 1 > scores[(j + u) % len(equipes)]):
-                        verif_ecart = False
-                if verif_ecart:
-                    fin = True
+            if scores[j] >= scoreLimit:
+                checkGap = True
+                for u in range(1, len(teams)):
+                    if not (scores[j] - 1 > scores[(j + u) % len(teams)]):
+                        checkGap = False
+                if checkGap:
+                    end = True
                     print("END OF THE GAME!!!")
-                    print(nomsEquipes[j], "are the best, CONGRATS!!!")
+                    print(teamNames[j], "are the best, CONGRATS!!!")
         elif word == None:
-            fin = True
+            end = True
             print("So sorry, end of the game, sadly no more words to play with")
         else:
             print(chr(27) + "[2J")
             timer()
             print("The word to guess was:", word)
-            suite = False
-            while not suite:
-                victoire = input("Good guess? (enter y or n) ")
-                if 'y' in victoire:
-                    suite = True
+            next = False
+            while not next:
+                victory = input("Good guess? (enter y or n) ")
+                if 'y' in victory:
+                    next = True
                     scores[j] += 1
                     i = i + 1
-                elif 'n' in victoire:
+                elif 'n' in victory:
                     i = i + 1
-                    suite = True
-            if scores[j] >= scoreLimite:
-                verif_ecart = True
-                for u in range(1, len(equipes)):
-                    if not (scores[j] - 1 > scores[(j + u) % len(equipes)]):
-                        verif_ecart = False
-                if verif_ecart:
-                    fin = True
+                    next = True
+            if scores[j] >= scoreLimit:
+                checkGap = True
+                for u in range(1, len(teams)):
+                    if not (scores[j] - 1 > scores[(j + u) % len(teams)]):
+                        checkGap = False
+                if checkGap:
+                    end = True
                     print("END OF THE GAME!!!")
-                    print(nomsEquipes[j], "are the best, CONGRATS!!!")
-    
-joueurs = ["Sofi", "Lucas", "Sara", "David"]
-nbEquipes = 2
+                    print(teamNames[j], "are the best, CONGRATS!!!")
 
-equipes = constitution_equipes(joueurs, nbEquipes)
-
-jeu(equipes)
+if __name__ == "__main__":
+    players = create_players()
+    teams = create_teams(players)
+    play(teams)
